6363a4208dd1c103087d20c038e76b7960cf463f
raghavTayal <raghavtayal2@gmail.com>
2021-01-21 20:18:28 +0530

    added invoice

--- cmd/invoices.go
+++ cmd/invoices.go
@@ -1,1 +1,51 @@
 package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/spf13/cobra"
+)
+
+type Invoice struct {
+}
+
+var InvoiceCmd = &cobra.Command{
+	Use:   "invoice",
+	Short: "print invoice",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		printInvoice(args)
+		return nil
+	},
+}
+
+var InvoiceId string
+
+func printInvoice(args []string) {
+
+	var response []byte
+
+	var err error
+
+	if InvoiceId == "" {
+		url := "https://api.razorpay.com/v1/invoices"
+		response, err = ExecuteRequest(url, http.MethodGet, nil)
+	} else {
+		url := "https://api.razorpay.com/v1/invoices/" + InvoiceId
+		response, err = ExecuteRequest(url, http.MethodGet, nil)
+	}
+	if err != nil {
+		fmt.Println(err)
+	}
+	var data map[string]interface{}
+
+	json.Unmarshal(response, &data)
+	result, _ := json.MarshalIndent(data, "", "  ")
+	fmt.Println(string(result))
+}
+
+func init() {
+	InvoiceCmd.Flags().StringVarP(&InvoiceId, "string", "i", "", "To Print invoice by id")
+	rootCmd.AddCommand(InvoiceCmd)
+}

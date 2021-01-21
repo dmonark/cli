7d5d979971c0f5d2069aedb5e3ad105308639a64
Jayesh Kumar <jayesh.kumar@razorpay.com>
2021-01-21 20:29:06 +0530

    payments route

--- cmd/payments.go
+++ cmd/payments.go
@@ -1,29 +1,60 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 
 	"github.com/spf13/cobra"
 )
 
+var paymentId string
+
 var paymentCmd = &cobra.Command{
-	Use:   "payments",
-	Short: "list all the payments",
+	Use:   "payment",
+	Short: "Payment Details for a payment id",
+	PreRun: validateAuth,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Println("Printing all the payments")
+		response, error := ExecuteRequest("http://api.razorpay.in:28080/v1/payments/"+paymentId, http.MethodGet, nil)
+		if error != nil {
+			fmt.Println(error.Error())
+		}
+		var data map[string]interface{}
+
+		fmt.Println("Printing the payment details for:- ", paymentId)
+
+		json.Unmarshal(response, &data)
+
+		fmt.Println(data)
+
 		return nil
 	},
 }
 
-var paymentCreateCmd = &cobra.Command{
-	Use:   "create",
-	Short: "create payment",
+var paymentListCmd = &cobra.Command{
+	Use:   "payments",
+	Short: "List of payments associated with this merchant",
+	PreRun: validateAuth,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Println("Creating payments")
+		response, error := ExecuteRequest("http://api.razorpay.in:28080/v1/payments", http.MethodGet, nil)
+		if error != nil {
+			fmt.Println(error.Error())
+		}
+		var data map[string]interface{}
+
+		fmt.Println("Listing the payments details")
+
+		json.Unmarshal(response, &data)
+
+		fmt.Println(data)
+
 		return nil
 	},
 }
 
 func init() {
-	paymentCmd.AddCommand(paymentCreateCmd)
+	paymentCmd.Flags().StringVarP(&paymentId, "id", "i", "", "Payment Id")
+	paymentCmd.MarkFlagRequired("id")
+	
+	paymentCmd.AddCommand(paymentListCmd)
 }

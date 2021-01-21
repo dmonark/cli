82410c7638096eccc0d4b1b77f89ab1e28d6bb8d
dedakiya.monark <dedakiya.monark@razorpay.com>
2021-01-21 21:29:21 +0530

    table added

--- cmd/orders.go
+++ cmd/orders.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 
+	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
 )
 
@@ -27,11 +29,64 @@ var orderCmd = &cobra.Command{
 	},
 }
 
-var orderCreateCmd = &cobra.Command{
-	Use:   "create",
-	Short: "create order",
+var orderListCmd = &cobra.Command{
+	Use:    "list",
+	Short:  "order list",
+	PreRun: validateAuth,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Println("Creating orders")
+		response, error := ExecuteRequest("http://0.0.0.0:28080/v1/orders", http.MethodGet, nil)
+		if error != nil {
+			fmt.Println(error.Error())
+		}
+		if response == nil {
+			fmt.Println("Empty Response")
+			os.Exit(1)
+		}
+		var data map[string]interface{}
+
+		json.Unmarshal(response, &data)
+
+		if data["count"].(float64) < 1 {
+			fmt.Println("No Entity found")
+			os.Exit(1)
+		}
+
+		table := tablewriter.NewWriter(os.Stdout)
+		table.SetHeader([]string{"ID", "Amount", "Currency", "Status", "Receipt", "Amount paid"})
+		table.SetHeaderColor(
+			tablewriter.Colors{tablewriter.Bold, tablewriter.FgGreenColor},
+			tablewriter.Colors{tablewriter.Bold, tablewriter.FgGreenColor},
+			tablewriter.Colors{tablewriter.Bold, tablewriter.FgGreenColor},
+			tablewriter.Colors{tablewriter.Bold, tablewriter.FgGreenColor},
+			tablewriter.Colors{tablewriter.Bold, tablewriter.FgGreenColor},
+			tablewriter.Colors{tablewriter.Bold, tablewriter.FgGreenColor},
+		)
+		table.SetColumnColor(
+			tablewriter.Colors{tablewriter.Bold, tablewriter.FgCyanColor},
+			tablewriter.Colors{tablewriter.Bold, tablewriter.FgCyanColor},
+			tablewriter.Colors{tablewriter.Bold, tablewriter.FgCyanColor},
+			tablewriter.Colors{tablewriter.Bold, tablewriter.FgCyanColor},
+			tablewriter.Colors{tablewriter.Bold, tablewriter.FgCyanColor},
+			tablewriter.Colors{tablewriter.Bold, tablewriter.FgCyanColor},
+		)
+		table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
+		table.SetAlignment(tablewriter.ALIGN_LEFT)
+
+		for _, v := range data["items"].([]interface{}) {
+			new_map := v.(map[string]interface{})
+			row := []string{
+				fmt.Sprintf("%v", new_map["id"]),
+				fmt.Sprintf("%v", new_map["amount"]),
+				fmt.Sprintf("%v", new_map["currency"]),
+				fmt.Sprintf("%v", new_map["status"]),
+				fmt.Sprintf("%v", new_map["receipt"]),
+				fmt.Sprintf("%v", new_map["amount_paid"]),
+			}
+			table.Append(row)
+		}
+
+		table.Render()
+
 		return nil
 	},
 }
@@ -39,5 +94,5 @@ var orderCreateCmd = &cobra.Command{
 func init() {
 	orderCmd.Flags().StringVarP(&orderId, "id", "i", "", "Order Id")
 	orderCmd.MarkFlagRequired("id")
-	orderCmd.AddCommand(orderCreateCmd)
+	orderCmd.AddCommand(orderListCmd)
 }
--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/fatih/color v1.10.0
 	github.com/gojektech/heimdall v5.0.2+incompatible
 	github.com/gojektech/valkyrie v0.0.0-20190210220504-8f62c1e7ba45 // indirect
+	github.com/olekukonko/tablewriter v0.0.4
 	github.com/spf13/cobra v1.1.1
 )

2b3c7d1522b8da3bd6f23ea1f26689ac32bbd735
dedakiya.monark <dedakiya.monark@razorpay.com>
2021-01-22 13:28:07 +0530

    dashboard and balance added

---
+++ cmd/dashboard.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"fmt"
+	"os/exec"
+	"runtime"
+
+	"github.com/spf13/cobra"
+)
+
+var dashboardListCmd = &cobra.Command{
+	Use:    "dashboard",
+	Short:  "open dashboard",
+	PreRun: validateAuth,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		url := "http://dashboard.razorpay.com/"
+		var err error
+
+		switch runtime.GOOS {
+		case "linux":
+			err = exec.Command("xdg-open", url).Start()
+		case "windows":
+			err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+		case "darwin":
+			err = exec.Command("open", url).Start()
+		default:
+			err = fmt.Errorf("unsupported platform")
+		}
+
+		return err
+	},
+}

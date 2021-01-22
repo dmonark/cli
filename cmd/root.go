951e1e2afbd0288cd1ede91e162bc7d8b230818d
dedakiya.monark <dedakiya.monark@razorpay.com>
2021-01-22 12:45:53 +0530

    api error mapping added

--- cmd/root.go
+++ cmd/root.go
@@ -13,11 +13,7 @@ var rootCmd = &cobra.Command{
 }
 
 func Execute() {
-<<<<<<< HEAD
-	rootCmd.AddCommand(paymentListCmd, orderListCmd, invoiceListCmd, authCmd, customerListCmd, disputeListCmd)
-=======
 	rootCmd.AddCommand(paymentListCmd, orderListCmd, InvoiceCmd, authCmd, customerListCmd)
->>>>>>> 07fa5d86eb76c03ee9b96474faae28475d743d18
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
